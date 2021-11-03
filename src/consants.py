a683ea40a6ed660e05aa674147cea21efe59cdb8
Michael Fisher <mefisher2@cougars.ccis.edu>
2021-11-03 14:34:24 -0500

    initial code for pygame project, created basic surface and display obj, defined constants

--- src/consants.py
+++ src/consants.py
@@ -1,8 +1,3 @@
 SIZE = width, height = 640, 480
 
 ## colors
-BLACK   = 0,0,0
-WHITE   = 255, 255, 255
-RED     = 255, 0, 0
-GREEN   = 0, 255, 0
-BLUE    = 0, 0, 255
--- src/main.py
+++ src/main.py
@@ -1,71 +1,46 @@
-import sys, pygame, consants
-from pygame import color
-from pygame.mixer import pause
-from pygame import Rect, event
+import pygame, sys
 from random import randint
 
-class play_object:
-    def __init__(self, surface, x, y, width, height, color = consants.BLUE) -> None:
+from pygame.rect import Rect
+
+import consants, colors
+
+
+# consants.SIZE = (1000, 1000)
+
+
+# x, y, width, height
+# test_obj = Rect(100, 100, 50, 50)
+
+class BasicRect:
+    def __init__(self, surface, x: float, y:float, w:float, h:float, color : tuple) -> None:
+        self.rect = Rect(x, y, w, h)
         self.surface = surface
-        self.rect = Rect(x, y, width, height)
         self.color = list(color)
-        self.speed = 3.14
-        self.timer = 0
-        self.dir = 0
 
-        self.__color_fade_choice = randint(0, 2)
-        self.__color_fade_value = 1
 
-    def move(self) -> None:
-
-        if self.timer >= 20:
-            self.timer = 0
-
-            self.dir = randint(0, 3)
-        else: self.timer += 1
-        print(self.timer)
-
-        if self.dir == 0 and self.rect.left >= 0: 
-            self.rect.x -= self.speed
-        if self.dir == 1 and self.rect.right <= consants.SIZE[0]: 
-            self.rect.x += self.speed
-        if self.dir == 2 and self.rect.top >= 0: 
-            self.rect.y -= self.speed
-        if self.dir == 3 and self.rect.bottom <= consants.SIZE[1]: 
-            self.rect.y += self.speed
-
-        return None
     def draw(self):
-        self.__color_fade()
-        print(self.color)
-        pygame.draw.rect(self.surface, self.color, test_obj)
+        pygame.draw.rect(self.surface, self.color, self.rect)
 
-    def __color_fade(self):
-        print(self.__color_fade_choice, self.color)
-        if self.color[self.__color_fade_choice] >= 250:
-            self.__color_fade_value = -1
-        if self.color[self.__color_fade_choice] <= 10:
-            self.__color_fade_value = 1
-        self.color[self.__color_fade_choice] += self.__color_fade_value
-            
+def main():
+    surface = pygame.display.set_mode(consants.SIZE)
+    pygame.display.set_caption("Test Project")
+
+    test_obj = BasicRect(surface, 100, 100, 50, 50, colors.BLUE)
+
+    while True:
+        ### Event Handling ###
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                print("quit code: ", pygame.QUIT)
+                sys.exit()
+
+        ### object handling
+
+        ### surface handling
+        surface.fill(colors.RED) # represent a color (0 - 255, 0 - 255, 0 - 255) -> r g b
+        test_obj.draw()
+        pygame.display.flip()
 
 
-screen = pygame.display.set_mode(consants.SIZE)
-pygame.display.set_caption('Encirle Python')
-test_obj = play_object(screen, 100, 100, 50, 50)
-clock = pygame.time.Clock()
-
-while 1:
-    for events in event.get():
-        if events.type == pygame.QUIT:
-            print(pygame.QUIT)
-            sys.exit()
-
-    test_obj.move()
-    
-    screen.fill(consants.RED)
-    # pygame.draw.rect(screen, consants.BLUE, test_obj)
-    test_obj.draw()
-    pygame.display.flip()
-
-    clock.tick(60)
+if __name__ == "__main__": main()
