0ac4651f0b4c6a180ac7b0253b8c9038040a70bc
Michael Fisher <mefisher2@cougars.ccis.edu>
2021-11-15 14:37:20 -0600

    fixed infinite lasers, added sprites for galaxian ships, discussed future goals

--- src/Laser.py
+++ src/Laser.py
@@ -11,34 +11,40 @@ class Laser(BaseObject):
         self.speed = 6.28
 
         self.alive = True
+        return None
 
-    def is_alive(self): return self.alive
-    def kill(self): self.alive = False
+    def is_alive(self) -> bool: return self.alive
+    def kill(self) -> None: self.alive = False
 
-    def draw(self):
+    def draw(self) -> None:
         pygame.draw.rect(self.surface.surface(), self.color, self.rect)
+        return None
 
-    def move(self):
+    def move(self) -> None:
         self.y -= self.speed
         self.rect.y = self.y
 
         if self.y < 0: self.kill()
+        return None
 
 class Lasers:
     def __init__(self) -> None:
         self.lasers = []
         return None
 
-    def draw(self):
+    def draw(self) -> None:
         for laser in self.lasers: laser.draw()
+        return None
 
-    def move(self):
+    def move(self) -> None:
         for laser in self.lasers: laser.move()
         self.lasers = [ laser for laser in self.lasers if laser.is_alive() ]
+        return None
 
     def add(self, laser: Laser) -> None:
         self.lasers.append(laser)
+        return None
 
-    def __len__(self): return len(self.lasers)
+    def __len__(self) -> int: return len(self.lasers)
 
     
